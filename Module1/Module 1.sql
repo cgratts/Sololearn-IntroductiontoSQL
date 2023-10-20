673470f7359444752733206ce86338837428e67b
cgratts <camgratts@yahoo.com>
2023-10-19 23:44:12 -0500

    Update Module 1.sql with SQL database and queries

--- Module1/Module 1.sql
+++ Module1/Module 1.sql
@@ -1,13 +1,41 @@
 -- Module 1: Getting Started with SQL
--- NOTE: Database used in this Module not available to run queries on 
+
+-- NOTE: The database/table used in this Module is not provided to run queries on 
+-- These were created in MySQL Workbench to simulate the course queries where applicable
 
 -- Lesson: Working with Data
 -- No code in this lesson
 
 -- Lesson: Running SQL Queries
 /*
+drop table if exists movies;
+create database if not exists test;
+use test;
+
+CREATE TABLE IF NOT EXISTS `movies` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `year` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
+
+INSERT INTO `movies` (`id`, `title`, `year`) VALUES
+(1, 'Home Alone', 1990),
+(2, 'Star Wars', 1977),
+(3, 'Jurassic Park', 1993),
+(4, 'Frozen', 2013),
+(5, 'Pirates of the Caribbean', 2003);
+
 SELECT year
 FROM movies
+
+Output:
+year
+1990
+1977
+1993
+2013
+2003
 */
 
 -- Lesson: Relational Databases
@@ -53,7 +81,33 @@ Complete the given query to extract the revenue field data from the table
 */
 
 /*
+drop table if exists movies;
+create database if not exists test;
+use test;
+
+CREATE TABLE IF NOT EXISTS `movies` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `revenue` int(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
+
+INSERT INTO `movies` (`id`, `title`, `revenue`) VALUES
+(1, 'Home Alone', 477000),
+(2, 'Star Wars', 775000),
+(3, 'Jurassic Park', 1030000),
+(4, 'Frozen', 1434000),
+(5, 'Pirates of the Caribbean', 963000);
+
 Solution: 
 SELECT revenue
 FROM movies
+
+Output:
+revenue
+477000
+775000
+1030000
+1434000
+963000
 */
